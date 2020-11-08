4c64bc6a9014781458dee1ca87b7d776a3615e31
stelisevil <stelisevil@hotmail.com>
2020-11-08 17:15:15 +0000

    fixed missing folder route name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Work from "./Work";
+import Work from "./work";
 
 const Home = () => {
   return (
