8262da49c6e08e6da10267c9548b906f6df83fb9
stelisevil <stelisevil@hotmail.com>
2020-11-09 09:57:00 +0000

    SSR styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,29 @@ import React from "react";
 import TopNav from "../components/TopNav";
 import "../styles/globals.css";
 
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3"
+  }
+};
+
 const MyApp = ({ Component, pageProps }) => (
   <>
+    <GlobalStyle />
     <TopNav />
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   </>
 );
 
