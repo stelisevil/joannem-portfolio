e1acd439295d5b0075e4fbd89ee01093457e6890
Stelios Tsinontas <41406137+stelisevil@users.noreply.github.com>
2021-03-30 20:35:46 +0100

    crate react app

--- pages/about-me/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const AboutMe = () => <h2>About me</h2>;
-
-export default AboutMe;
--- pages/work/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Work = () => <h2>Work</h2>;
-
-export default Work;
