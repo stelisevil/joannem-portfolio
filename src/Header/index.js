0fe53a2431437c4e0c95513c390e31bad6825be3
Stelios Tsinontas <41406137+stelisevil@users.noreply.github.com>
2021-04-06 23:55:29 +0100

    initial copy

---
+++ src/Header/index.js
@@ -0,0 +1,8 @@
+const Header = () => (
+  <div className="w-full">
+    <div className="flex justify-center text-6xl my-12">Joanne McGarvie</div>
+    <hr />
+  </div>
+);
+
+export default Header;
---
+++ src/Intro/index.js
@@ -0,0 +1,22 @@
+const Intro = () => (
+  <div className="text-center w-1/2">
+    <div className="my-6 mt-12 text-3xl">Hi, I’m Joanne.</div>
+    <div className="my-4 text-2xl">
+      I’m a digital designer who loves to create new ideas.
+    </div>
+    <div className="my-2">
+      With over ten years’ professional experience, my skills span pretty much
+      every element of digital design, from UX/UI, to video editing for social
+      media, to HTML/CSS, to graphic design and art direction. Not knowing how
+      to do something isn’t a barrier – I’m a fast learner, and my knowledge has
+      constantly evolved during my career.
+    </div>
+    <div className="my-2 font-bold">
+      I’m in the process of building my own website from scratch, but this is a
+      temporary portfolio in the meantime!
+    </div>
+    <div className="my-2">Below are some examples of my work…</div>
+  </div>
+);
+
+export default Intro;
